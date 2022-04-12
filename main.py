93a68aa7ea1632dac2bda9967bbcca56461dabd9
Mitch A <alves.mitch@gmail.com>
2022-04-12 13:17:01 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+    https://github.com/pwaller/pyfiglet
+
+    Font Types:
+    - 3x5
+    - 5lineoblique
+    - alligator
+    - alphabet
+    - banner3-D
+    - bubble
+    - bulbhead
+    - digital
+    - doh
+    - dotmatrix
+    - isometric1
+    - letters
+    - 
+"""
+
+
+import pyfiglet
+  
+result = pyfiglet.figlet_format("DATA-BIT", font = "slant"  )
+print(result)
